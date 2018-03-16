a48d0988cbd4751381cd69140302a56cc6c974dd
George Lubomirov <GLubomirov@abilitics.com>
2018-03-16 10:51:59 +0200

    Changed the way Certificate Renewal Script is Called.
    Now we use threads to call the Certificate Renewal Script

--- Check_IIS_Certificate_Expiration.ps1
+++ Check_IIS_Certificate_Expiration.ps1
@@ -1,4 +1,4 @@
-﻿function checkForCertificateExpiration($daysExpiry){
+﻿function checkForCertificateExpiration($daysExpiry, $renewCerts){
     ####Check if Script is Ran as Administrator. Otherwize IIS info is not available.
     If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){    
         write-host "This script needs to be run As Administrator" -ForegroundColor Red
@@ -15,7 +15,7 @@
     $bindings = Get-WebBinding | where {$_.protocol -eq "https"}
 
     foreach($binding in $bindings){
-        ####Get Certificate for the Binding from Certificate Store. Try Web Hosting Store first and if the Cert is not there try the Personal Stor
+        ####Get Certificate for the Binding from Certificate Store. Try Web Hosting Store first and if the Cert is not there try the Personal Store
         $obj = dir "Cert:LocalMachine\WebHosting" -recurse | where {$_.Thumbprint -eq $binding.certificateHash} 
 
         if(!($obj)){
@@ -46,16 +46,108 @@
         }
     }
 
-    ####Display Results
-    #$warningForExpiration | out-gridview
+    if($renewCerts -eq "true"){
+        ############################################
+        ##Prepare Multi-threading
+        ############################################
 
-    foreach($cert in $warningForExpiration){
-        $bindingRegex = ($cert.BindingInformation -split "443:")[1]
-        $siteName = $cert.SiteName
+        ####Create an empty array for multi-threading
+        $RunspaceCollection = @()
 
-        ####Call Cetificate Renewal Script
-        ."C:\PATHTOSCRIPT\Lets-Encrypt_Automate_PowerShell.ps1" -domain $bindingRegex -iisSiteName $siteName -renew "True"
+        ####This is the array we want to ultimately add our information to
+        [Collections.Arraylist]$dataFull = @()
+
+        ####Create the sessionstate variable entry
+        $varPass = New-object System.Management.Automation.Runspaces.SessionStateVariableEntry -ArgumentList 'varPass',$Form,$Null
+
+        $InitialSessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+
+        ####Variables passed to every Runspace. Not Used, left for reference.
+        #$bindingRegex = [System.String]::Empty
+        #$siteName = [System.String]::Empty
+
+        ####Add the variable to the sessionstate. Not Used, left for reference.
+        #$InitialSessionState.Variables.Add($bindingRegex)
+        #$InitialSessionState.Variables.Add($siteName)
+
+        ####Create a Runspace Pool with a minimum and maximum number of run spaces. (http://msdn.microsoft.com/en-us/library/windows/desktop/dd324626(v=vs.85).aspx)
+        ####Edit how many sessions will be open - this is effectivelly how many users access the system at any time
+        $RunspacePool = [RunspaceFactory]::CreateRunspacePool(1,1,$InitialSessionState, $host)
+
+        ####Open the RunspacePool
+        $RunspacePool.Open()
+
+        $ScriptBlock = {
+            Param($bindingRegex, $siteName)
+            ."C:\PS_Cert\certReq.ps1" -domain $bindingRegex -iisSiteName $siteName -renew "True"
+        }
+
+        ############################################
+        ##Start Certificate Issuance 
+        ############################################
+        foreach($cert in $warningForExpiration){
+            $bindingRegex = ($cert.BindingInformation -split "443:")[1]
+            $siteName = $cert.SiteName
+
+            ####You can exclude sites here
+            #if($siteName -notlike "domain.com"){
+                $Powershell = [PowerShell]::Create().AddScript($ScriptBlock).AddArgument($bindingRegex).AddArgument($siteName)
+
+                $Powershell.RunspacePool = $RunspacePool
+
+                [Collections.Arraylist]$RunspaceCollection += New-Object -TypeName PSObject -Property @{
+		            Runspace = $PowerShell.BeginInvoke()
+		            PowerShell = $PowerShell  
+	            }
+            #}        
+        }
+
+        ############################################
+        ##Dispose of Threads
+        ############################################
+
+        ####Here we collect the Data and dispose of the Runspaces
+        While($RunspaceCollection) {
+	
+	        ####Just a simple ForEach loop for each Runspace to get resolved
+	        Foreach ($Runspace in $RunspaceCollection.ToArray()) {
+		
+		        ####Here's where we actually check if the Runspace has completed
+		        If ($Runspace.Runspace.IsCompleted) {
+			
+			        ####Since it's completed, we get our results here
+			        [void]$dataFull.Add($Runspace.PowerShell.EndInvoke($Runspace.Runspace))
+			
+			        ####Here's where we cleanup our Runspace
+			        $Runspace.PowerShell.Dispose()
+			        $RunspaceCollection.Remove($Runspace)
+			
+		        } #/If
+	        } #/ForEach
+        } #/While
+
+    } else {
+
+        $notificationMail = @()
+        if($warningForExpiration){
+            foreach($cert in $warningForExpiration){
+                $bindingRegex = ($cert.BindingInformation -split "443:")[1]
+                $siteName = $cert.SiteName
+
+                ####You can exclude sites here
+                ##if($siteName -notlike "domain.com"){
+                    $notificationMail+=$bindingRegex
+                ##} 
+            }
+        }
+        
+        if($notificationMail){
+            $notificationMail | out-gridview
+        }
+    
     }
 }
 
-checkForCertificateExpiration -daysExpiry 2
+##Renew
+checkForCertificateExpiration -daysExpiry 3 -renewCerts "true"
+
